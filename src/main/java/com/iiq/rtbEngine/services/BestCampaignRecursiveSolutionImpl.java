51b2c8364a7fd1229a9778db053e1ce14be6d9f9
Eliezer <reuvene@mprest.com>
2020-12-08 19:33:16 +0200

    any changes

--- src/main/java/com/iiq/rtbEngine/services/BestCampaignRecursiveSolutionImpl.java
+++ src/main/java/com/iiq/rtbEngine/services/BestCampaignRecursiveSolutionImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import java.util.Set;
 
 @Component
-public class BestCampaignRecursiveSolutionQueueImpl extends AbstractBestCampaignSolution {
+public class BestCampaignRecursiveSolutionImpl extends AbstractBestCampaignSolution {
 
 
     @Override
